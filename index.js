a4ca8619eb774099a30a0f44e3fcf861c0e688e2
CamiloCastellanosc <camiloalejo89@hotmail.com>
2023-09-06 19:52:55 -0500

    emote

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const connection = require("./src/db.js");
 
 connection();
 server.listen(PORT, () => {
-    console.log("Servidor levantado con exito en el puerto", PORT);
+    console.log("Servidor levantado con exito en el puerto ", PORT ," 👀 ");
 });
